de27a2c6a9ec980caf1333815719f0b0f0082d8b
Zarlanga <robertozitto@gmail.com>
2022-12-24 00:22:38 +0000

    creado cookies rudimentarias de sesion y habilitado el espacio para el form de nuevo posteo

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const la66 = require('./src/routers/router.js')
 const path = require('path');
+const fileUpload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+
+//onda que todo esto lo puse al pedo(o no?), ver despues como desinstalarlos
+const cors = require('cors');
+//const bodyParser = require('body-parser');
+//const morgan = require('morgan');
+//const _ = require('lodash');
+
+
+
 const app = express();
 
 
@@ -9,9 +20,19 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './src/views'));
 
 //pa la data de las request (por ej post)
-app.use(express.urlencoded());
+//app.use(bodyParser.urlencoded({extended: true}));
+//app.use(bodyParser.json());
+app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
+//pa el file upload
+app.use(cors());
+//app.use(morgan('dev'));
+app.use(fileUpload({
+    createParentPath: true
+}));
+
+app.use(cookieParser());
 
 app.use(express.static("./pajitas"))
 
--- src/controllers/general.js
+++ src/controllers/general.js
@@ -1,13 +1,33 @@
+//const fileUpload = require('express-fileupload');
 const fs = require("fs")
 const path = require('path');
 
-var contador = 0
+
 var db = fs.readFileSync(path.resolve(__dirname,'../data/test.json')) ;
 db = JSON.parse(db);
 
+var contador = 0;
+var indexImg = fs.readdirSync(path.resolve(__dirname, '../../pajitas/imgs/') ).length
+
+/*
+TODO:
+-express-fileupload
+-ver como chequear que sea archivo de imagen
+(preferentemente en el front?)
+-ver como o donde poner el contador de imagenes
+  const pe = fs.readdirSync(path.resolve(__dirname, '../../pajitas/imgs/') )
+  console.log("+" + pe)
+  console.log("-" + pe.length)
+
+
+*/
 
 function pappo (req,res) { 
+  //res.cookie("test", [4,5],  { secure: true, httpOnly: true, sameSite: 'strict'})
+  //console.log(req.cookies.test[1])
+  //res.clearCookie("test")
   res.end("anduvo bien")
+  
 }
 
 const logg =  (req, res, next) =>{
@@ -32,8 +52,9 @@ const fetchh = (req,res) => {
 
 const template =  (req,res) => { 
   var user = db.find((r) => r.id == req.params.id  )
+  var editable = req.params.id == req.cookies.usuario
   
-  res.render(path.resolve(__dirname, '../views/template'), {user: user} )
+  res.render(path.resolve(__dirname, '../views/template'), {user: user, editable: editable} )
 }
 
 const postComment = (req, res) => {
@@ -60,13 +81,35 @@ const createUserForm = (req, res) => {
 }
 
 const createUserPost= (req, res) => {
+  /*tudu:
+  -chequear que el usuario no exista previamente (eh nuevo no?)
+      -(hacer el response para el caso negativo)
+  -chequear que el archivo sea imagen
+      -ver ut supra
+  -cheuqear tamaño de imagen y comprimirlo si hace falta
+*/
+  const term = req.files.logo.name.slice(-4);
+    
   const user = req.body;
   user.comments= [];
   user.items = [];
-  db.push(user)
-  fs.writeFileSync(path.resolve(__dirname, '../data/test.json'), JSON.stringify(db, null, ' ')); 
-  //res.send(req.body)
-  res.redirect("@"+user.id)
+  user.logo = indexImg + term
+  
+  if(db.find( i => i.id == user.id)) {
+    res.end("usuario existente, pruebe otro nombre de usuario")
+  } else {
+    
+    req.files.logo.mv(`${path.resolve(__dirname, '../../pajitas/imgs/')}/${indexImg}.jpg`)
+    
+    
+    db.push(user)
+    
+    fs.writeFileSync(path.resolve(__dirname, '../data/test.json'), JSON.stringify(db, null, ' ')); 
+    
+    indexImg++;
+    res.cookie("usuario", user.id,  { maxAge: 400 * 24 * 60 * 60 * 1000, secure: true, httpOnly: true, sameSite: 'strict'})
+    res.redirect("@"+user.id)
+  }
 
 }
 module.exports ={
